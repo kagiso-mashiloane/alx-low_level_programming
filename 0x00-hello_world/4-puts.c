5d8ffb502520faad5f415d629df4d7583f31cdbf
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-06-15 16:31:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * My first c programming
- *
  * Return: 0
 */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
