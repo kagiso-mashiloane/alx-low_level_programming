630dffbf6f5e6e892c86014220e91b5a0f259606
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-06-15 18:44:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * Return: 1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora kopar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora kopar, 2015-10-19\n", 59);
 	return (1);
 }
