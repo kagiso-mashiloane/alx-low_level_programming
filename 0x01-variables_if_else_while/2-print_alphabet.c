ba6344c78ea3f7953aa65bd22b2af192d8e1c5e4
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-06-17 04:01:08 +0200

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main- Entry point
@@ -6,12 +7,12 @@
  * Description: Print alphabet lowercase
  * Return: Zero value
  */
-
+void trim(char i)
 int main(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 'a';i <='z';i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
