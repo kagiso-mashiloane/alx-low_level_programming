fdc4db8b745a36ca6adee2c5c1a4b45d3db11785
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-06-20 01:58:45 +0200

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/*
- * main - Entry point
- * Description: 'prints all single digit numbers of base 10'
- * Return: Always 0
+
+/**
+ * main- Entry point
+ * @void: Null value
+ * Description: Print numbers from 0 to 9
+ * Return: Zero value
  */
+
 int main(void)
 {
 	int n;
