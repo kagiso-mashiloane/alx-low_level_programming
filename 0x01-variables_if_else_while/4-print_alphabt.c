47906a0426af738af2347009195c533b76043a2a
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-06-20 01:26:53 +0200

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *  *main-Entry point
+ *   *@void:Null value
+ *    *Description:Print alphabet  except q and e
+ *     *Return:Always 0(sucess)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		if (i == 101 || i == 113)
+		{
+			continue;
+			i++;
+		}
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
