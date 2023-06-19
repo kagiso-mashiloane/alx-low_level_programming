f58d28a7d4cdc7c33ffb6d024e6652b6bc80c603
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-06-20 01:42:30 +0200

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Description: prints all single digit numbers of base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 0; i++)
+	{
+		printf("%d", i);
+	}
+	printf("/n");
+	return (0);
+}
