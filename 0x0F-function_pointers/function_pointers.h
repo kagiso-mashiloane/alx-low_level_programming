cca1b65351a3e88ad481639367f82af84880243f
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-07-20 01:20:41 +0200

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stddef.h>
+
+int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
+
+#endif
