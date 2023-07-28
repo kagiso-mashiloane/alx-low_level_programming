0c6a06e8656494fc5407e96e905ef91407afa6e5
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-07-28 03:29:10 +0200

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,18 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t No_nodes = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		No_nodes++;
+		nodes++;
 		h = h->next;
 	}
-	return (No_nodes);
+	return (nodes);
 }
