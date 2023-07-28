e8bf5be0e1c84484d8aad771b5ebf83f9911cafb
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-07-28 03:56:55 +0200

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,18 +2,18 @@
 /**
  * list_len - Finds the number of elements in
  * a linked list_t list.
- * @h: The linked list_t list.
- * Return:  number of elements in,
+ * @h: number of elements in list_t list.
+ * Return: the number of elements in,
  * a linked list_t list.
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t elements_no = 0;
 
 	while (h)
 	{
-		elements++;
+		elements_no++;
 		h = h->next;
 	}
-	return (elements);
+	return (elements_no);
 }
