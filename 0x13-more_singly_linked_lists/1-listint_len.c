ba669c1bcf43554703923012a5e7dad67aa984a6
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-07-31 21:42:29 +0200

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * listint_len -returns the number of elements in a linked listint_t list.
- * @h: the type of list listint_t
+ * @h: the type of list listint_t.
  * Return: the number of nodes
  */
 size_t listint_len(const listint_t *h)
