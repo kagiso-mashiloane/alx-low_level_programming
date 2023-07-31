e1523252807a5830d593c36788bd6eb9169b16ac
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2023-08-01 00:16:51 +0200

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index - that reverses a listint_t linked list.
+ * delete_nodeint_at_index -  that deletes the node at index,
+ * index of a listint_t linked list
  * @head: listint_t linked list
- * @index: the node delete
- * Return: a pointer to the first node of the reversed list
+ * @index:  is the index of the node that should,
+ * be deleted. Index starts at 0
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
